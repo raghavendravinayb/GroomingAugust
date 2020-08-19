8ed041868472c182fbc674a319f36a55c52bee03
Raghav <Raghav@192.168.1.5>
2020-08-19 13:17:57 +0530

    Project Commit

---
+++ Flipkart/src/main/java/com/flipkart/genericLib/BaseTest.java
@@ -0,0 +1,5 @@
+package com.flipkart.genericLib;
+
+public class BaseTest {
+
+}
---
+++ Flipkart/src/test/java/com/flipkart/Search/SearchProductTest.java
@@ -0,0 +1,16 @@
+package com.flipkart.Search;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SearchProductTest {
+	
+	@Test
+	public void run() {
+		Reporter.log("SearchProductTest Successfully running",true);
+		Reporter.log("SearchProductTest Successfully running",true);
+		Reporter.log("SearchProductTest Successfully running",true);
+		Reporter.log("SearchProductTest Successfully running",true);
+		Reporter.log("SearchProductTest Successfully running",true);
+	}
+}
